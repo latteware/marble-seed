9cc6a8c699d1631d3d7cb1b2626fb118304af28d
Irvin Contreras <irvincnt93@gmail.com>
2018-03-05 06:43:42 -0600

    add addon Info

--- stories/components/panelDisplay/panelDisplay.stories.js
+++ stories/components/panelDisplay/panelDisplay.stories.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 import PanelDisplayData from './../../../lib/frontend/components/base-panelDisplay'
 
 storiesOf('Panel', module)
-  .add('Display Data', () => <PanelDisplayData />)
+  .add('Display Data', 
+    withInfo(`
+      Description and documentation
+      ~~~js
+      <Button>Click Here</Button>
+      ~~~
+    `)(() => <PanelDisplayData />))
